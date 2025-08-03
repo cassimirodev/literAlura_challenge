e66a32acaae398288ddbd9e0b38cb9dcaa8763f1
Luís Eduardo <luiseduardocass06@gmail.com>
2025-08-03 15:24:43 -0300

    feat: add repository

--- src/main/java/com/alura/LiteraArula/BookRepository/BookRepository.java
+++ src/main/java/com/alura/LiteraArula/BookRepository/BookRepository.java
@@ -2,7 +2,12 @@ package com.alura.LiteraArula.BookRepository;
 
 import com.alura.LiteraArula.model.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
 
+    Optional<BookEntity> findByTitulo(String titulo);
 }
--- src/main/java/com/alura/LiteraArula/BookRepository/PersonRepository.java
+++ src/main/java/com/alura/LiteraArula/BookRepository/PersonRepository.java
@@ -1,7 +1,12 @@
 package com.alura.LiteraArula.BookRepository;
 
 import com.alura.LiteraArula.model.PersonEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
+    Optional<PersonEntity> findByNome(String nome);
 }
